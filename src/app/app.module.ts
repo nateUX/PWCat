ba2198518a3abd7c13356c7bcd46f80b95c24055
nateUX <nathan.mitchell@ni.com>
2018-04-22 15:26:48 -0500

    adding service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
 import { MatToolbarModule, MatCardModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
@@ -16,7 +19,8 @@ import { ImgCardComponent } from './img-card/img-card.component';
     BrowserModule,
     MatToolbarModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
   ],
   providers: [],
   bootstrap: [AppComponent]
