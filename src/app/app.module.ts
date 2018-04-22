ee305e0010be0eb23b0cd3ca3bc440f3fed2e40c
nateUX <nathan.mitchell@ni.com>
2018-04-22 15:16:52 -0500

    adding some whitespace to pass tslint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MatToolbarModule,MatCardModule,MatButtonModule } from '@angular/material';
+import { MatToolbarModule, MatCardModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { ImgCardComponent } from './img-card/img-card.component';
